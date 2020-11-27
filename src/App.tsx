1c0318ee9a2eb3bf3ac3f99f99836135fba26b1c
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-26 23:10:03 -0300

    chore: set editorconfig, prettier and eslint to the project

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <span>src/App.tsx</span>
+          and save to reload.
         </p>
         <a
           className="App-link"
