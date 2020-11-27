0df8e0c94e851d60e0c0c723b45bdb2999a23375
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-26 23:13:21 -0300

    chore: refact the directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <span>src/App.tsx</span>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => <div>Hello</div>;
 
 export default App;
